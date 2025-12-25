a97784f6ecfbf0f3feca20fea4e87adacd5ac412
ezio <ya817412@gmail.com>
2025-12-25 19:04:17 +0100

    Add an deployement api

--- src/store/ticketsSlice.js
+++ src/store/ticketsSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://694d7777ad0f8c8e6e20c266.mockapi.io/tickets",
 });
 
 const ticketsSlice = createSlice({
